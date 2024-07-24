124df4e92390f856623d8dcfdf01411fb8d867fb
Evan Cook <el.8oney@gmail.com>
2024-07-24 13:39:03 -0700

    added a dealer and a simple hand view

--- Deck.cs
+++ Deck.cs
@@ -1,8 +1,7 @@
-using Godot;
 using System;
 using System.Collections.Generic;
 
-public partial class Deck : Node
+public partial class Deck
 {
 	public List<Card> cards = new List<Card>();
 	public Random rng = new Random();
@@ -18,25 +17,18 @@ public partial class Deck : Node
 		}
 	}
 
-    public override void _Ready()
-    {
-        Populate_Deck();
-		//Shuffle_Deck();
-		Card middle_card = cards[26];
-		GD.Print(middle_card);
-		Cut_Deck_By_Card(middle_card);
-		//Cut_Deck_By_Position(26);
-		Print_Deck();
-		
-    }
-
-	void Print_Deck()
+	public Deck()
 	{
-		foreach(Card c in cards)
-		{
-			GD.Print(c);
-		}
+		Populate_Deck();
 	}
+
+	// void Print_Deck()
+	// {
+	// 	foreach(Card c in cards)
+	// 	{
+	// 		GD.Print(c);
+	// 	}
+	// }
 
 	public void Shuffle_Deck()
 	{
@@ -51,18 +43,18 @@ public partial class Deck : Node
         }
 	}
 
-	int Find_Card(Card desired_card)
+	public int Find_Card(Card desired_card)
 	{
 		return cards.FindIndex(desired_card.Equals);
 	}
 
-	void Cut_Deck_By_Card(Card card)
+	public void Cut_Deck_By_Card(Card card)
 	{
 		int cut_position = Find_Card(card);
 		Cut_Deck_By_Position(cut_position);
 	}
 
-	void Cut_Deck_By_Position(int cut_position)
+	public void Cut_Deck_By_Position(int cut_position)
 	{
 		List<Card> cut_cards = cards.GetRange(0,cut_position);
 		cards.RemoveRange(0,cut_position);
