de5b35b8021e1147474cafc2b078a1d2d0efc9b8
Evan Cook <El.8oney@gmail.com>
2025-12-14 20:55:27 -0800

    Updated to Godot 4.5.1 stable

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -14,14 +14,12 @@ public partial class Card
 	public int value;
 
 	Suits GetSuit()
-	{
-		return suit;
-	}
+	{ return suit; }
 
 	public Card(int value = 14, Suits suit = Suits.Spades)
 	{
-		
-		this.value = Math.Clamp(value,2, 14);
+
+		this.value = Math.Clamp(value, 2, 14);
 		this.suit = suit;
 	}
 
@@ -29,14 +27,14 @@ public partial class Card
 	{
 		get
 		{
-			string value_name = null;
-			switch(value)
+            string value_name;
+            switch (value)
 			{
 				case 11:
 					value_name = "Jack";
 					break;
 				case 12:
-					value_name  = "Queen";
+					value_name = "Queen";
 					break;
 				case 13:
 					value_name = "King";
@@ -57,16 +55,17 @@ public partial class Card
 		return Face_Name;
 	}
 
-    public override bool Equals(object obj)
-    {
-        if(obj is Card c)
+	public override bool Equals(object obj)
+	{
+		if (obj is Card c)
 		{
-			if(value == c.value && suit == c.suit)
-			{return true;}
+			if (value == c.value && suit == c.suit)
+			{ return true; }
 		}
 		return false;
-    }
+	}
 
-    public static bool operator ==(Card c1, Card c2){return c1.Equals(c2);}
-	public static bool operator !=(Card c1, Card c2){return !c1.Equals(c2);}
+	public static bool operator ==(Card c1, Card c2) { return c1.Equals(c2); }
+	public static bool operator !=(Card c1, Card c2) { return !c1.Equals(c2); }
+
 }
